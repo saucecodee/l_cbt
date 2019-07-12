7e075a4c876b1720514e1e95acd6b66e9643ff8e
Goodnessokoye <goodnessokoye10@gmail.com>
2019-07-12 18:22:57 +0100

    this is the question and welcome function

--- config.js
+++ config.js
@@ -19,7 +19,69 @@ var config = require('./config')
 //===============================================================================
 
 //========== Questions array ==========
-const questions = []
+const questions = [ questions = [
+     {
+         question: "what is physics all about?",
+         option: {
+             a: "fire",
+             b: "book",
+             c: "science",
+             d: "all of the above"
+         },
+         answer: [option.c] 
+         
+     },
+ 
+     {
+         question: "what is geography about?",
+         option: {
+             a: "fire",
+             b: "earth",
+             c: "water",
+             d: "none of the above"
+         },
+         answer: b
+         
+     },
+ 
+     {
+         question: "what is biology all about?",
+         option: {
+             a: "tea",
+             b: "kerosine",
+             c: "stone",
+             d: "none of the above"
+         },
+         answer: d
+         
+     },
+ 
+    {
+     question: "what is agriculture all about?",
+     option:{
+         a: "human body",
+         b: "plant and animal",
+         c: "none",
+         d:"all of the above",
+ 
+         },
+         answer: b
+ 
+    },
+    
+    {
+        question: "what is chemistry all about?",
+        option:{
+        a: "Atoms,element...",
+        b: "stone",
+        c: "all of the above",
+        d: "none "
+        },
+        answer:a
+    }
+ 
+ ]
+ ]
 
 //========== Answers array ==========
 var answers = [];
