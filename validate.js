872e4375d82eabfcae109fcf12e160a6fd18140a
Vicman2 <vicmanthebest@gmail.com>
2019-07-12 17:55:21 +0100

    The validation script by Victor

--- validate.js
+++ validate.js
@@ -5,8 +5,23 @@
 //========== require global variables ==========
 const config = require('./config');
 
-function validate () {
+//----------------------------
 
+
+
+function validate (index, userAns) {
+
+    if(index >= 0 && (index >questions.length -1)){    //validate the index
+        let realAns = questions[index].answer;
+        if(realAns == userAns){
+            answers[index] +=1;  
+        }
+    }
+
+    
+    goToQuestion();
+
+    //
 }
 
 module.exports = validate
